6208df061b203eb8a66dcfd873b5b3840a1fc640
Konrad <konszewczyk@outlook.com>
2022-03-17 15:49:32 +0100

    Extended value selection methods for Uniform

--- src/pipeline/transforms.py
+++ src/pipeline/transforms.py
@@ -1,6 +1,6 @@
 import os
 from abc import ABC, abstractmethod
-from typing import Dict, Union
+from typing import Dict, Union, Callable
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -62,15 +62,25 @@ class UniformSelectionTransform(Transform):
     """
     A class that limits the number of voxels in the blob by either selecting  This class extends 'Transformer' class.
 
-    :param config: configuration dictionary with integer 'max_voxel' and boolean 'mean_selection' entries
+    :param config: configuration dictionary with integer 'max_voxel' and str 'method' entries
     """
 
     def __init__(self, config: Union[Dict, None] = None, **kwargs) -> None:
         super().__init__(config, **kwargs)
         if self.__dict__.get('max_voxel') is None:
             raise ValueError("{} requires 'max_voxel' key in config dictionary".format(type(self).__name__))
-        if self.__dict__.get('mean_selection') is None:
-            raise ValueError("{} requires 'mean_selection' key in config dictionary".format(type(self).__name__))
+        if self.__dict__.get('method') is None:
+            raise ValueError("{} requires 'method' key in config dictionary".format(type(self).__name__))
+        self._selection = self._selection_method(self.method)
+
+    @staticmethod
+    def _selection_method(selection: str) -> Callable:
+        methods = {
+            'basic': UniformSelectionTransform._basic_selection,
+            'average': UniformSelectionTransform._average_selection,
+            'max': UniformSelectionTransform._max_selection
+        }
+        return methods[selection]
 
     @staticmethod
     def _pad_blob(blob: np.ndarray, scale: int) -> np.ndarray:
@@ -85,6 +95,41 @@ class UniformSelectionTransform(Transform):
         nonzero = np.array(np.nonzero(blob))
         return nonzero.shape[-1]
 
+    @staticmethod
+    def _basic_selection(blob: np.ndarray, scale: int) -> np.ndarray:
+        voxel_samples = blob[scale // 2::scale, scale // 2::scale, scale // 2::scale]
+        processed_blob = np.zeros(blob.shape)
+        processed_blob[scale // 2::scale, scale // 2::scale, scale // 2::scale] = voxel_samples
+        return processed_blob
+
+    @staticmethod
+    def _average_selection(blob: np.ndarray, scale: int) -> np.ndarray:
+        padded_blob = UniformSelectionTransform._pad_blob(blob, scale)
+        sub_arrays = []
+        for (x, y, z) in itertools.product(list(range(scale)), repeat=3):
+            sub_array = padded_blob[x::scale, y::scale, z::scale]
+            sub_arrays.append(sub_array)
+        sub_arrays = np.stack(sub_arrays)
+        voxel_samples = np.average(sub_arrays, axis=0)
+        processed_blob = np.zeros(padded_blob.shape)
+        processed_blob[scale // 2::scale, scale // 2::scale, scale // 2::scale] = voxel_samples
+        processed_blob = processed_blob[:blob.shape[0], :blob.shape[1], :blob.shape[2]]
+        return processed_blob
+
+    @staticmethod
+    def _max_selection(blob: np.ndarray, scale: int) -> np.ndarray:
+        padded_blob = UniformSelectionTransform._pad_blob(blob, scale)
+        sub_arrays = []
+        for (x, y, z) in itertools.product(list(range(scale)), repeat=3):
+            sub_array = padded_blob[x::scale, y::scale, z::scale]
+            sub_arrays.append(sub_array)
+        sub_arrays = np.stack(sub_arrays)
+        voxel_samples = np.max(sub_arrays, axis=0)
+        processed_blob = np.zeros(padded_blob.shape)
+        processed_blob[scale // 2::scale, scale // 2::scale, scale // 2::scale] = voxel_samples
+        processed_blob = processed_blob[:blob.shape[0], :blob.shape[1], :blob.shape[2]]
+        return processed_blob
+
     def preprocess(self, blob: np.ndarray) -> np.ndarray:
         nonzero = self._nonzero(blob)
         if nonzero <= self.max_voxel:
@@ -93,21 +138,7 @@ class UniformSelectionTransform(Transform):
         scale = math.ceil(scale)
         processed_blob = blob
         while self._nonzero(processed_blob) > self.max_voxel:
-            if self.mean_selection:
-                padded_blob = self._pad_blob(blob, scale)
-                sub_arrays = []
-                for (x, y, z) in itertools.product(list(range(scale)), repeat=3):
-                    sub_array = padded_blob[x::scale, y::scale, z::scale]
-                    sub_arrays.append(sub_array)
-                sub_arrays = np.stack(sub_arrays)
-                voxel_samples = np.average(sub_arrays, axis=0)
-                processed_blob = np.zeros(padded_blob.shape)
-                processed_blob[scale // 2::scale, scale // 2::scale, scale // 2::scale] = voxel_samples
-                processed_blob = processed_blob[:blob.shape[0], :blob.shape[1], :blob.shape[2]]
-            else:
-                voxel_samples = blob[scale // 2::scale, scale // 2::scale, scale // 2::scale]
-                processed_blob = np.zeros(blob.shape)
-                processed_blob[scale // 2::scale, scale // 2::scale, scale // 2::scale] = voxel_samples
+            processed_blob = self._selection(blob, scale)
             scale += 1
         return processed_blob
 
@@ -122,6 +153,6 @@ if __name__ == "__main__":
     files = os.listdir("../../data/blobs_full")[12]
     blob = np.load(f"../../data/blobs_full/{files}")["blob"]
     print("Pre-size: {}".format(UniformSelectionTransform._nonzero(blob)))
-    transform = UniformSelectionTransform({'max_voxel': 2000, 'mean_selection': False})
+    transform = UniformSelectionTransform({'max_voxel': 2000, 'method': 'max'})
     transformed = transform.preprocess(blob)
     print("Post-size: {}".format(UniformSelectionTransform._nonzero(transformed)))
