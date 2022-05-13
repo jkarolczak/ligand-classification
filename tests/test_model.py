a29650d4bc66a0007fac017f8de5f397b0952cf8
jkarolczak <jacek.karolczak@outlook.com>
2022-05-13 22:57:38 +0200

    Add tests for softmax

---
+++ tests/test_model.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+import MinkowskiEngine as ME
+
+import models
+from cfg import read_config
+from data import LigandDataset
+
+
+@pytest.fixture
+def batch():
+    x = torch.ones((4, 8, 8, 8), dtype=torch.float32)
+    coords, feats = LigandDataset._get_coords_feats(x)
+    return ME.SparseTensor(feats, coords.contiguous())
+
+
+def test_softmax(batch):
+    cfg = read_config("../cfg/train.yaml")
+    model = models.create(cfg["model"])
+    y_hat = model(batch)
+    assert y_hat.sum(-1).mean() == 1
