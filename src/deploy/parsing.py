c2cfa317c65d4dffcb6b8b09cf97b165947c122c
Jacek Karolczak <jacek.karolczak@outlook.com>
2022-12-22 21:03:23 +0100

    Add support for .npy

--- src/deploy/parsing.py
+++ src/deploy/parsing.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 def parse_npz(byte_obj: _io.BytesIO) -> np.ndarray:
     """
-    Parse point cloud saved as compressed numpy array.
+    Parse point cloud saved as a compressed numpy array.
 
     :param byte_obj: input object to be parsed.
     :type byte_obj: _io.BytesIO
@@ -17,6 +17,19 @@ def parse_npz(byte_obj: _io.BytesIO) -> np.ndarray:
     blob_container = np.load(byte_obj, encoding="bytes")
     blob_name = blob_container.__dict__["files"][-1]
     return blob_container[blob_name]
+
+
+def parse_npy(byte_obj: _io.BytesIO) -> np.ndarray:
+    """
+    Parse point cloud saved as a numpy array.
+
+    :param byte_obj: input object to be parsed.
+    :type byte_obj: _io.BytesIO
+    :returns: point cloud
+    :rtype: np.ndarray
+    """
+    blob = np.load(byte_obj, encoding="bytes")
+    return blob
 
 
 def parse(file: st.runtime.uploaded_file_manager.UploadedFile) -> np.ndarray:
@@ -29,4 +42,9 @@ def parse(file: st.runtime.uploaded_file_manager.UploadedFile) -> np.ndarray:
     :rtype: np.ndarray
     """
     byte_object = BytesIO(file.getvalue())
-    return parse_npz(byte_object)
+
+    ext = file.__dict__["name"].split(".")[-1]
+    if ext == "npz":
+        return parse_npz(byte_object)
+    elif ext == "npy":
+        return parse_npy(byte_object)
