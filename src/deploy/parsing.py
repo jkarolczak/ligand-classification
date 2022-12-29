3b871a0a5f6923e8d8e67127263dc2e1f40030fa
annprzy <anneprzybylowska@gmail.com>
2022-12-29 17:55:31 +0100

    add ply parser

--- src/deploy/parsing.py
+++ src/deploy/parsing.py
@@ -3,6 +3,7 @@ from io import BytesIO
 
 import numpy as np
 import streamlit as st
+import plyfile as ply
 
 
 def parse_npz(byte_obj: _io.BytesIO) -> np.ndarray:
@@ -32,6 +33,40 @@ def parse_npy(byte_obj: _io.BytesIO) -> np.ndarray:
     return blob
 
 
+def parse_ply(byte_obj: _io.BytesIO) -> np.ndarray:
+    """
+    ply file structure:
+     x y z feature
+     where x, y, z are coordinates and feature is a feature value at point with the given x, y, z coordinates
+    :param byte_obj: input object to be parsed.
+    :type byte_obj: _io.BytesIO
+    :returns: point cloud
+    :rtype: np.ndarray
+
+    """
+    plydata = ply.PlyData.read(byte_obj)
+
+    properties_names = [str(p.name) for p in plydata.elements[0].properties]
+
+    x = np.array(plydata.elements[0].data[properties_names[0]])[:, np.newaxis]
+    y = np.array(plydata.elements[0].data[properties_names[1]])[:, np.newaxis]
+    z = np.array(plydata.elements[0].data[properties_names[2]])[:, np.newaxis]
+    features = np.array(plydata.elements[0].data[properties_names[-1]])[:, np.newaxis]
+
+    x_range = int(np.max(x)) + 1
+    y_range = int(np.max(y)) + 1
+    z_range = int(np.max(z)) + 1
+
+    blob = np.zeros((x_range, y_range, z_range))
+    blob[(x.astype(int), y.astype(int), z.astype(int))] = features
+
+    return blob
+
+
+def parse_pcd(byte_obj: _io.BytesIO) -> np.ndarray:
+    pass
+
+
 def parse(file: st.runtime.uploaded_file_manager.UploadedFile) -> np.ndarray:
     """
     Parse point cloud.
@@ -48,3 +83,7 @@ def parse(file: st.runtime.uploaded_file_manager.UploadedFile) -> np.ndarray:
         return parse_npz(byte_object)
     elif ext == "npy":
         return parse_npy(byte_object)
+    elif ext == 'ply':
+        return parse_ply(byte_object)
+    elif ext == 'pcd':
+        return parse_pcd(byte_object)
