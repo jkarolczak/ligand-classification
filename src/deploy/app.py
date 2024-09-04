6994299b895c38c2312c52f17dc3977de76ca78d
Jacek Karolczak <jacek.karolczak@outlook.com>
2024-09-04 20:23:20 +0200

    Move number_input back to form context

--- src/deploy/app.py
+++ src/deploy/app.py
@@ -90,25 +90,24 @@ def main():
     with col1_form:
         file_val = st.file_uploader("Input", type=["npy", "npz", "ply", "pts", "xyz", "txt", "csv"])
         rescale_cryoem = st.toggle("Rescale voxel values (turn this on for cryoEM blobs)")
-
-        if rescale_cryoem:
-            resolution = st.number_input("Resolution", min_value=1.0, max_value=4.0, value=1.0, step=0.1)
+        resolution = st.number_input("Resolution (used only for cryoEM blobs)", min_value=1.0, max_value=4.0, value=1.0,
+                                     step=0.1)
 
         submit_button = st.form_submit_button("Submit")
 
-    if submit_button:
-        if file_val:
-            blob = parse(file_val)
-            if rescale_cryoem:
-                blob = scale_cryoem_blob(blob, resolution=resolution)
-            viz = volume_3d(blob, "Blob")
-            col1_content.plotly_chart(viz, use_container_width=True, height=500)
-            blob = preprocess(blob)
-            preds = predict(blob, model)
-            with col2_predictions.container():
-                st.components.v1.html(preds, width=650, height=1000, scrolling=True)
-        else:
-            col2_predictions.error("No file has been uploaded.")
+        if submit_button:
+            if file_val:
+                blob = parse(file_val)
+                if rescale_cryoem:
+                    blob = scale_cryoem_blob(blob, resolution=resolution)
+                viz = volume_3d(blob, "Blob")
+                col1_content.plotly_chart(viz, use_container_width=True, height=500)
+                blob = preprocess(blob)
+                preds = predict(blob, model)
+                with col2_predictions.container():
+                    st.components.v1.html(preds, width=650, height=1000, scrolling=True)
+            else:
+                col2_predictions.error("No file has been uploaded.")
 
 
 if __name__ == "__main__":
