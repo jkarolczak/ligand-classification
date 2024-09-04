8585e95bee69ce32c52485793ffd01b00371bbae
jkarolczak <59308844+jkarolczak@users.noreply.github.com>
2024-09-04 20:38:59 +0200

    App cryoem scaling (#134)
    * Add scalling cryoem
    
    * Move number_input out of form context
    
    * Move number_input back to form context
    
    * Move number_input back to form context
    
    * Move number_input back to form context
    
    * Fix preprocessing key
    
    * Split resolution inputs into two columns
    
    * Split resolution inputs into two columns
    
    * Split resolution inputs into two columns

--- src/deploy/app.py
+++ src/deploy/app.py
@@ -4,9 +4,8 @@ import streamlit as st
 
 from deploy.inference import predict, load_model
 from deploy.parsing import parse
-from deploy.preprocessing import preprocess
+from deploy.preprocessing import preprocess, scale_cryoem_blob
 from deploy.visualization import volume_3d
-
 
 model = None
 
@@ -90,9 +89,21 @@ def main():
     col2_predictions.info("Upload a blob to see predictions.")
     with col1_form:
         file_val = st.file_uploader("Input", type=["npy", "npz", "ply", "pts", "xyz", "txt", "csv"])
-        if st.form_submit_button():
+        form_col1, form_col2 = col1_form.columns(2)
+        with form_col1:
+            rescale_cryoem = st.toggle("Rescale voxel values (turn this on for cryoEM blobs)")
+
+        with form_col2:
+            resolution = st.slider("Resolution (used only for cryoEM blobs)", min_value=1.0, max_value=4.0, value=1.0,
+                                   step=0.1)
+
+        submit_button = st.form_submit_button("Submit")
+
+        if submit_button:
             if file_val:
                 blob = parse(file_val)
+                if rescale_cryoem:
+                    blob = scale_cryoem_blob(blob, resolution=resolution)
                 viz = volume_3d(blob, "Blob")
                 col1_content.plotly_chart(viz, use_container_width=True, height=500)
                 blob = preprocess(blob)
--- src/deploy/preprocessing.py
+++ src/deploy/preprocessing.py
@@ -3,6 +3,39 @@ import numpy as np
 from pipeline.transforms import UniformSelectionTransform
 
 transformation = None
+MAP_VALUE_MAPPER = {
+    1.0: 0.66,
+    1.1: 0.63,
+    1.2: 0.57,
+    1.3: 0.57,
+    1.4: 0.54,
+    1.5: 0.50,
+    1.6: 0.48,
+    1.7: 0.44,
+    1.8: 0.42,
+    1.9: 0.39,
+    2.0: 0.36,
+    2.1: 0.33,
+    2.2: 0.31,
+    2.3: 0.30,
+    2.4: 0.28,
+    2.5: 0.25,
+    2.6: 0.25,
+    2.7: 0.23,
+    2.8: 0.21,
+    2.9: 0.21,
+    3.0: 0.20,
+    3.1: 0.18,
+    3.2: 0.18,
+    3.3: 0.17,
+    3.4: 0.15,
+    3.5: 0.16,
+    3.6: 0.14,
+    3.7: 0.12,
+    3.8: 0.14,
+    3.9: 0.15,
+    4.0: 0.17,
+}
 
 
 def get_transformation() -> UniformSelectionTransform:
@@ -19,3 +52,10 @@ def preprocess(blob: np.ndarray) -> np.ndarray:
     transformation = get_transformation()
     blob = transformation.preprocess(blob)
     return blob
+
+
+def scale_cryoem_blob(blob: np.ndarray, resolution: float) -> np.ndarray:
+    """
+
+    """
+    return blob * (MAP_VALUE_MAPPER[round(resolution, 1)] / blob[blob > 0].min())
